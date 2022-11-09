bcb34c39a7872a0b2266962647d6c5d8d6cfc90c
ChihYuLin <chihyulin66@gmail.com>
2022-11-09 22:29:29 +0800

    Day 8. Authentication-1 登入註冊

--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,7 @@ Route::group(['prefix' => 'routeGroups', 'as' => 'routeGroups' ], function() {
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
