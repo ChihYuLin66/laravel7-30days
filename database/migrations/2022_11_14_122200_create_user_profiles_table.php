5d5380444737339b6e0342d04756a204eb2e9e65
ChihYuLin <chihyulin66@gmail.com>
2022-11-14 22:01:28 +0800

    Day 11. 建立資料表: Migration #1還有model

---
+++ database/migrations/2022_11_14_122200_create_user_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->tinyInteger('gender')->nullable()->comment('姓別');
+            $table->date('birthday')->nullable()->comment('生日');
+            $table->string('address', 200)->nullable()->comment('地址');
+            $table->timestamps();
+        });
+
+        DB::statement("ALTER TABLE `user_profiles` comment '使用者基本資料'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
