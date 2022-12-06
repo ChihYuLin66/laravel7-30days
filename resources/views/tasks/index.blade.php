591d4c62e65715e36e650380ae28a89138129a5b
ChihYuLin <chihyulin66@gmail.com>
2022-12-06 23:05:40 +0800

    Day 24. 實作 TODO 練習 下篇:畫面及功能

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,30 +7,52 @@
             <div class="card">
                 <div class="card-header">ToDo List</div>
                 <div class="card-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">任務</th>
-                                <th scope="col">操作</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if ($tasks->isNotEmpty())
-                                @foreach ($tasks as $task)
+                    <div class="row">
+                        <div class="col-md-12 mb-1">
+                            <a href="{{ route('tasks.create') }}">
+                                <button class="btn btn-primary">create</button>
+                            </a>
+                        </div>
+                        <div class="col-md-12">
+                            <table class="table">
+                                <thead>
                                     <tr>
-                                        <th scope="row">{{ $loop->iteration }}</th>
-                                        <td>{{ $task->title }}</td>
-                                        <td></td>
+                                        <th scope="col">#</th>
+                                        <th scope="col">任務</th>
+                                        <th scope="col">操作</th>
                                     </tr>
-                                @endforeach
-                            @else
-                                <tr>
-                                    <td colspan="3" class="text-center">No Data</td>
-                                </tr>
-                            @endif
-                        </tbody>
-                    </table>
+                                </thead>
+                                <tbody>
+                                    @if ($tasks->isNotEmpty())
+                                        @foreach ($tasks as $task)
+                                            <tr>
+                                                <th scope="row">{{ $loop->iteration }}</th>
+                                                <td>{{ $task->title }}</td>
+                                                <td>
+                                                    <form action="{{ route('tasks.destroy', $task->id) }}" method="POST">
+                                                        @csrf
+                                                        @method('delete')
+
+                                                        <a href="{{ route('tasks.edit', $task->id) }}">
+                                                            <button class="btn btn-outline-dark" type="button">edit</button>
+                                                        </a>
+                                                        
+                                                        <a href="javascript:;">
+                                                            <button class="btn btn-outline-danger" type="submit">delete</button>
+                                                        </a>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    @else
+                                        <tr>
+                                            <td colspan="3" class="text-center">No Data</td>
+                                        </tr>
+                                    @endif
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>                    
                 </div>
             </div>
         </div>
